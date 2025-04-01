139019c35bf62859924161fc0a071286e78aff4c
Sarath3551E <141985903+Sarath3551E@users.noreply.github.com>
2025-04-01 14:42:27 +0530

    Update integration.test.js

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,20 +1,17 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const expect = chai.expect;
 
 chai.use(chaiHttp);
-const { expect } = chai;
 
-describe('Integration Test', function () {
-    this.timeout(5000); // Ensure timeout is sufficient for server response
-
-    it('should return a 200 status for the homepage', function (done) {
-        chai.request('http://localhost:8080') 
-            .get('/')
-            .end((err, res) => {
-                if (err) return done(err);
-                expect(res).to.have.status(200);
-                expect(res).to.be.json; // Ensure response is in JSON format (optional)
-                done();
-            });
-    });
+describe('Integration Test: Nginx Web Server', () => {
+  it('should return status 200 and the greeting from the HTML file', (done) => {
+    chai.request('http://localhost:8080&#39;) 
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);  
+        expect(res.text).to.include('Hello from Docker!');  
+        done();
+      });
+  });
 });
