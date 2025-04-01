6ad88b8021694dee8b96b7b366cfbe866b164124
Sarath3551E <141985903+Sarath3551E@users.noreply.github.com>
2025-04-01 14:37:04 +0530

    Update integration.test.js

--- test/integration.test.js
+++ test/integration.test.js
@@ -5,12 +5,15 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Integration Test', function () {
+    this.timeout(5000); // Ensure timeout is sufficient for server response
+
     it('should return a 200 status for the homepage', function (done) {
-        chai.request('http://localhost:8080') // Make sure this URL is properly formatted
+        chai.request('http://localhost:8080') 
             .get('/')
             .end((err, res) => {
                 if (err) return done(err);
                 expect(res).to.have.status(200);
+                expect(res).to.be.json; // Ensure response is in JSON format (optional)
                 done();
             });
     });
